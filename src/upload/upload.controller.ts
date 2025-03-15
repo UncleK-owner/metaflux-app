5ebdc817dca41fd833f438f945270bbf5636cd6f
UncleK <starkapin@gmail.com>
2025-03-15 19:43:56 +0900

    Update .gitignore, upgrade Prettier to 3.5.3, and enhance file upload handling

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,29 +1,43 @@
-import { Controller, Post, UseInterceptors, UploadedFile, ParseFilePipe, MaxFileSizeValidator, FileTypeValidator } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UseInterceptors,
+  UploadedFile,
+  ParseFilePipe,
+  MaxFileSizeValidator,
+  FileTypeValidator,
+} from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
-import { extname } from 'path';
+import { extname, parse } from 'path';
 
 @Controller('upload')
 export class UploadController {
   @Post()
-  @UseInterceptors(FileInterceptor('file', {
-    storage: diskStorage({
-      destination: './uploads',
-      filename: (req, file, callback) => {
-        const name = file.originalname.split('.')[0];
-        const ext = extname(file.originalname);
-        const filename = `${name}_${Date.now()}${ext}`;
-        callback(null, filename);
-      },
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, callback) => {
+          const { name, ext } = parse(file.originalname);
+          const filename = `${encodeURIComponent(name)}_${Date.now()}${ext}`; // 파일 이름 인코딩
+          callback(null, filename);
+        },
+      }),
     }),
-  }))
-  uploadFile(@UploadedFile(new ParseFilePipe({
-    validators: [
-      new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 5 }), // 5MB 제한
-    //   new FileTypeValidator({ fileType: '.(png|jpg|jpeg|gif|csv)' }),
-    ],
-  })) file: Express.Multer.File) {
+  )
+  uploadFile(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 5 }), // 5MB 제한
+          //   new FileTypeValidator({ fileType: '.(png|jpg|jpeg|gif|csv)' }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
     console.log(file);
-    return { message: 'File uploaded successfully!', filename: file.filename };
+    return { message: 'File uploaded successfully!', filename: decodeURIComponent(file.filename) };
   }
 }
